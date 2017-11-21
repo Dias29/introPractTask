5cfa32f86bdb5e6fa7fd5fd80e72806951d07b71
Dias29 <33831288+Dias29@users.noreply.github.com>
2017-11-21 21:35:59 +0600

    did pract task

---
+++ Lesson 1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1
+            Console.WriteLine("Hello!");
+            Console.WriteLine("My name is Dias!");
+            Console.ReadLine();
+
+
+            //Задание 2
+            Console.Write("Введите первое число: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            int y = int.Parse(Console.ReadLine());
+            int sum = x + y;
+            Console.Write("Сумма двух чисел равна - " + sum);
+            Console.Read();
+             
+        }
+    }
+}
---
+++ Lesson 1/Program1.cs
@@ -0,0 +1,18 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Lesson_1
+//{
+//    class Program1
+//    {
+//        static int Main()
+//        {
+
+
+//            return 0;
+//        }
+//    }
+//}
